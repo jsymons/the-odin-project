3e1478b62aea10cdb9c7bcd3ddbe33782a4f5815
Jason Symons <jason.symons@gmail.com>
2015-07-19 12:15:06 -0400

    ruby-building-blocks: stock_picker - refactored code

--- ruby-building-blocks/stock_picker.rb
+++ ruby-building-blocks/stock_picker.rb
@@ -1,25 +1,18 @@
 def stock_picker(stock_prices)
-	if stock_prices.index(stock_prices.min) < stock_prices.index(stock_prices.max)
-		[stock_prices.index(min),stock_prices.index(max)]
-	else
-		buy = stock_prices.sort
-		sell = stock_prices.sort.reverse
-		profits = Hash.new(0)
-
-		buy.each do |low|
-			sell.each do |high|
-				low_index = stock_prices.index(low)
-				high_index = stock_prices.index(high)
-				if low_index < high_index && high - low > 0
-					profits[[low_index, high_index]] = high - low
-				end
+	buy_day = 0
+	sell_day = 0
+	profit = 0
+	days = 0..stock_prices.length - 1
+		
+	for x in days
+		for y in days
+			if x < y && stock_prices[y] - stock_prices[x] > profit
+				buy_day, sell_day, profit = x, y, stock_prices[y] - stock_prices[x]
 			end
 		end
-
-		profits = profits.sort_by {|days, profit| profit}.reverse.to_h
-
-		profits.keys.first
 	end
+
+	[buy_day, sell_day]
 end
 
 test_prices = [17,3,6,9,15,8,6,1,10]
